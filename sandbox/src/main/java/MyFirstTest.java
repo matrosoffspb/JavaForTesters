d0fe3b756b371988a03d6654da44dc4b4af79602
Maksim Lyamin <mnlyamin@mail.ru>
2016-08-12 21:50:43 +0300

    Created project for IDEA

--- sandbox/src/main/java/MyFirstTest.java
+++ sandbox/src/main/java/MyFirstTest.java
@@ -1,6 +1,6 @@
 public class MyFirstTest {
-	
-	public static void main(String[] args){
-		System.out.println ("Hello, my frends!");
-	}
+
+    public static void main(String[] args) {
+        System.out.println("Hello, my frends!");
+    }
 }
