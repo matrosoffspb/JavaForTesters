999bbca55aaa70181b259de5018ad1da0063225e
Maksim Lyamin <mnlyamin@mail.ru>
2016-08-14 22:05:51 +0300

    Added methods and classes

---
+++ sandbox/src/main/java/com/lesson1/hometask1/Square.java
@@ -0,0 +1,14 @@
+package com.lesson1.hometask1;
+
+public class Square {
+
+    public double l;
+
+    public Square(double len) {
+        this.l = len;
+    }
+
+    public double area() {
+        return this.l * this.l;
+    }
+}
