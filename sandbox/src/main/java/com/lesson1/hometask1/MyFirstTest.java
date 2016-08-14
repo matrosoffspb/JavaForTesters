5787d77889ad25aff5d486771769993513b2bb43
Maksim Lyamin <mnlyamin@mail.ru>
2016-08-14 21:12:59 +0300

    Added some functions

--- sandbox/src/main/java/com/lesson1/hometask1/MyFirstTest.java
+++ sandbox/src/main/java/com/lesson1/hometask1/MyFirstTest.java
@@ -3,10 +3,27 @@ package com.lesson1.hometask1;
 public class MyFirstTest {
 
     public static void main(String[] args) {
-        hello();
+        hello("frends");
+        hello("child");
+        hello("wife");
+        hello("mother");
+        hello("father");
 
+        double len = 5;
+        System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
     }
-    public static void hello(){
-        System.out.println("Hello, my frends!");
+    public static void hello(String anybody){
+        System.out.println("Hello, my " + anybody + "!");
+    }
+
+    public static double area (double l){
+        return l*l;
+    }
+    public static double area (double a, double b){
+        return a * b;
     }
 }
