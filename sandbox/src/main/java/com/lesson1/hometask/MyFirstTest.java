90e87c9224cb67a3882957783e6217f78dacb30e
Maksim Lyamin <mnlyamin@mail.ru>
2016-08-14 23:16:54 +0300

    Method distance move to class Point

--- sandbox/src/main/java/com/lesson1/hometask/MyFirstTest.java
+++ sandbox/src/main/java/com/lesson1/hometask/MyFirstTest.java
@@ -17,15 +17,10 @@ public class MyFirstTest {
 
         Point p1 = new Point(1,1);
         Point p2 = new Point(4,3);
-        
-        System.out.println("Расстояние между точками: " + distance(p1, p2));
+        System.out.println("Расстояние между точками: " + p2.distance(p1,p2));
 
     }
     public static void hello(String anybody){
         System.out.println("Hello, my " + anybody + "!");
     }
-    public static double distance(Point p1, Point p2){
-        return Math.sqrt( Math.pow(p2.x-p1.x, 2) + Math.pow(p2.y-p1.y, 2) );
-    }
-
 }
