c3b0a9e049a3e2791387b3a1e1fb8dbe1a88ea29
Maksim Lyamin <mnlyamin@mail.ru>
2016-08-14 23:05:35 +0300

    Fixed class Point

--- sandbox/src/main/java/com/lesson1/hometask/Point.java
+++ sandbox/src/main/java/com/lesson1/hometask/Point.java
@@ -5,10 +5,8 @@ public class Point {
     public double x;
     public double y;
 
-    public Point(double p1, double p2) {
-        this.x = p1;
-        this.y = p1;
-        this.x = p2;
-        this.y = p2;
-    }
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+        }
 }
