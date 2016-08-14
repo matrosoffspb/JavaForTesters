0616dbe28c01c6cf2c573587fc87071f10bb66e1
Maksim Lyamin <mnlyamin@mail.ru>
2016-08-14 20:45:16 +0300

    Added package and added function

--- sandbox/src/main/java/com/lesson1/hometask1/MyFirstTest.java
+++ sandbox/src/main/java/com/lesson1/hometask1/MyFirstTest.java
@@ -1,6 +1,12 @@
+package com.lesson1.hometask1;
+
 public class MyFirstTest {
 
     public static void main(String[] args) {
+        hello();
+
+    }
+    public static void hello(){
         System.out.println("Hello, my frends!");
     }
 }
