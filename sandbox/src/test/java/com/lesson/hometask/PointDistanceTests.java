6b474cddd34a6529c66ca40889d2e39d2957b307
matrosoffspb <mnlyamin@mail.ru>
2016-08-20 22:07:10 +0300

    Added new test to DistanceTests

--- sandbox/src/test/java/com/lesson/hometask/PointDistanceTests.java
+++ sandbox/src/test/java/com/lesson/hometask/PointDistanceTests.java
@@ -24,4 +24,13 @@ public class PointDistanceTests {
         Assert.assertEquals(p2.distance(p1), 5.656, "This is wrong distance between points");
 
     }
+
+    @Test
+    public void PointDistance2(){
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(3,5);
+
+        Assert.assertEquals(p2.distance(p1), 4.47213595499958, "This is distance between points");
+
+    }
 }
