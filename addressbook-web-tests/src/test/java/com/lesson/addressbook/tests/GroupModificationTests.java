793ba60774c309b1ea56d257c4d86ca9108b29f6
Maksim Lyamin <mnlyamin@mail.ru>
2016-09-17 16:22:52 +0300

    Updated Modification Group

--- addressbook-web-tests/src/test/java/com/lesson/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/com/lesson/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import com.lesson.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -12,13 +13,18 @@ public class GroupModificationTests extends TestBase {
     public void testGroupModification(){
         checkCreationGroup();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().selectGroup(0);
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test1m", "test2m", "test3m"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1m", "test2m", "test3m");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size(), "Check count groups" );
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after), "Check list");
     }
 
 }
