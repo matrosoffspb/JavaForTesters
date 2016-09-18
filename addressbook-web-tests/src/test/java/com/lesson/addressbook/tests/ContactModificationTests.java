9e1662a968c3a5bae888475b63da3229df9c3279
Maksim Lyamin <mnlyamin@mail.ru>
2016-09-19 00:44:54 +0300

    Error

--- addressbook-web-tests/src/test/java/com/lesson/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/com/lesson/addressbook/tests/ContactModificationTests.java
@@ -4,6 +4,7 @@ import com.lesson.addressbook.model.ContactData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactModificationTests extends TestBase {
@@ -15,11 +16,16 @@ public class ContactModificationTests extends TestBase {
             app.getContactHelper().createContact(new ContactData("Bob", "Marly", "Dilan", "bobby77", "Job", "Anything address", "999-00-00", "bobby77@pro.com", "facebook.com", "test1"));
         }
         List<ContactData> before = app.getContactHelper().getContactList();
-        app.getContactHelper().editContact(0);
-        app.getContactHelper().fillContactForm(new ContactData("Bob1", "Marly1", "Dilan1", "bobby77", "Job", "Anything address", "999-00-00", "bobby77@pro.com", "facebook.com", null), false);
+        app.getContactHelper().editContact(before.size() - 1);
+        ContactData contact = new ContactData("Bob1", "Marly1", "Dilan1", "bobby77", "Job", "Anything address", "999-00-00", "bobby77@pro.com", "facebook.com", null);
+        app.getContactHelper().fillContactForm((contact), false);
         app.getContactHelper().updateContact();
         app.getNavigationHelper().returnHomePage();
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size(), "Check list contacts");
+
+        before.remove(before.size() - 1);
+        before.add(contact);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
