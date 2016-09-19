62f3669bd6b888a0574e2d8b583208c6627bd44c
Maksim Lyamin <mnlyamin@mail.ru>
2016-09-19 22:35:27 +0300

    Added new method compare contacts

--- addressbook-web-tests/src/test/java/com/lesson/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/com/lesson/addressbook/model/ContactData.java
@@ -14,7 +14,7 @@ public class ContactData {
     private String group;
 
     public ContactData(String firstname, String middlename, String lastname, String nickname, String company, String address, String homephone, String email, String homepage, String group) {
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
         this.firstname = firstname;
         this.middlename = middlename;
         this.lastname = lastname;
@@ -105,7 +105,6 @@ public class ContactData {
 
         ContactData that = (ContactData) o;
 
-        if (id != that.id) return false;
         if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
         return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
 
@@ -113,8 +112,7 @@ public class ContactData {
 
     @Override
     public int hashCode() {
-        int result = id;
-        result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
+        int result = firstname != null ? firstname.hashCode() : 0;
         result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
         return result;
     }
