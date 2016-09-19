303e0cf0c58873c1d9350f2b832c445bc5629769
Maksim Lyamin <mnlyamin@mail.ru>
2016-09-20 00:18:27 +0300

    Added Suite

--- addressbook-web-tests/src/test/java/com/lesson/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/com/lesson/addressbook/tests/TestBase.java
@@ -4,18 +4,20 @@ import com.lesson.addressbook.appmanager.ApplicationManager;
 import com.lesson.addressbook.model.GroupData;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
-    @BeforeMethod
+    @BeforeSuite
     public void setUp() throws Exception {
         app.init();
     }
 
-    @AfterMethod
+    @AfterSuite
     public void tearDown() {
         app.stop();
     }
